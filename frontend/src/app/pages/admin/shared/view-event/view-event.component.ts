45923876361492e878da60c85bc72b107d2db2a1
Ram Krishna Dumrakoti <krishna01992@gmail.com>
2024-04-25 19:44:36 +0545

    event added and holiday mapped

---
+++ frontend/src/app/pages/admin/shared/view-event/view-event.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-event',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-event.component.html',
+  styleUrl: './view-event.component.scss'
+})
+export class ViewEventComponent {
+
+}
